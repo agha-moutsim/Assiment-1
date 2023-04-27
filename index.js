0ed055ddc0967bea80bcce68775b6d5bedc81544
aghamoutsim <aghamoutsim@gmail.com>
2023-04-27 13:57:22 +0500

    ad

---
+++ index.js
@@ -0,0 +1,20 @@
+// alert("Welcome to JS Land... Happy Coding");
+
+// alert("John Doe");
+
+// alert("15 Years Old");
+
+var a = 12;
+var b = 9;
+
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+var j = a % b;
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(j);
