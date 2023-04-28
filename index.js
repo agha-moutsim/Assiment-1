5950664f0d55c61235b3b3774e2a26a7bac01517
aghamoutsim <aghamoutsim@gmail.com>
2023-04-28 06:49:24 +0500

    Assignment 1

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// alert("Welcome to JS Land... Happy Coding");
+alert("Welcome to JS Land... Happy Coding");
 
-// alert("John Doe");
+alert("John Doe");
 
-// alert("15 Years Old");
+alert("15 Years Old");
+
+alert("Free fire")
+
+alert("God of War Ragnarok");
+
+var agha = 98
+var moustim = 67
+
+console.log(agha);
+console.log(Hello);
 
 var a = 12;
 var b = 9;
@@ -18,3 +28,73 @@ console.log(d);
 console.log(e);
 console.log(f);
 console.log(j);
+
+var a = 9
+var b = 78
+var c = 89
+var d = 23
+var e = 3
+var f = 68
+var j = 46
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(j);
+  
+
+var a = 7;
+var b = prompt("What is your age?");
+var c =parseInt(b);
+console.log(b);
+
+var a = 4;
+++a;
+console.log(a);
+
+var b = 23;
+--b;
+console.log(b);
+
+var c=22;
+var f=140;
+c = (f - 32) * 5 / 9;
+console.log(c);
+ f = (c * 9 / 5) + 32;
+console.log(f);
+
+var us = 10 * 155; 
+var sr = 25 * 41;
+var pkr = us + sr;
+
+console.log(pkr);
+
+var a = 16;
+++a;
+--a;
+++a;
+--a;
+console.log(a);
+
+var b = 89;
+--b;
+++b;
+--b;
+++b;
+console.log(b);
+
+var a = 1;
+var b = 2;
+--a;
+--a - --b;
+--a - --b+ ++b;
+--a - --b+ ++b + b--;
+console.log(a);
+console.log(b);
+
+
+
+
